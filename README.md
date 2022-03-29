3eaba57a1c741b6e472fc16068442242cce1938c
Rubens-filho <fracicorubens@yahoo.com>
2022-03-29 20:44:45 -0300

    atualizacoes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Porfolio Pessoal
 
 # Feito com React.js, HTML, CSS e JavaScript. Totalmente responsivo e dinâmico.
+
+
+![Portfolio Website](https://i.ibb.co/WgPMpts/image.png)
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,6 +8,24 @@ export const projects = [
     repositorio: 'https://github.com/rubbinhoxd/portifolio',
     id: 0,
   },
+  {
+    title: 'DtMoney',
+    description: "As tecnologias usadas foram HTML, CSS, Typescript e react.js. Meu primeiro projeto feito com Typescript. É um aplicativo de controle financeiro, aonde o usuário final pode ver como está o seu saldo atual.",
+      image: '/images/2.png',
+      tags: ['HTML', 'CSS', 'Typescript', 'React'],
+    site: '',
+    repositorio: 'https://github.com/rubbinhoxd/dtMoney_ignite',
+    id: 1,
+  },
+  {
+    title: 'Rocketshoes',
+    description: "As tecnologias usadas foram HTML, CSS, Typescript e react.js. É um projeto aonde basicamente faz um hook de carrinho de compras. Aonde nele, o usuário final pode comprar apenas um, ou mais de um par de tênis.Foi feito também com um software que simula uma API (Json Server) e um banco de dados (LocalStorage API). Usei também uma lib do React chamada de toastify, aonde mostra informações temporárias e animadas.",
+      image: '/images/3.png',
+      tags: ['HTML', 'CSS', 'Typescript', 'React'],
+    site: '',
+    repositorio: 'https://github.com/rubbinhoxd/desafio02Rocketseat',
+    id: 1,
+  },
 ];
 
 export const TimeLineData = [
